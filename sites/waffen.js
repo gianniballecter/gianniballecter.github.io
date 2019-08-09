2d8916b764679164d15910c09f1d1a12d1449cb0
gianniballecter <46056629+gianniballecter@users.noreply.github.com>
2019-08-09 22:59:22 +0200

    Create waffen.js

---
+++ sites/waffen.js
@@ -0,0 +1,39 @@
+var weapon =new Array(); 
+
+weapon[0] = {
+    name: "default",
+    schaden: 3,
+    angriff: 2,
+    standardmoves: [0, 1, 2],
+    wmoves: []
+}
+
+weapon[42] = {
+    name: "Trollkeule",
+    schaden: 3,
+    angriff: 2,
+    standardmoves: [0, 1, 2],
+    wmoves: []
+}
+
+/*****************************************************ANGRIFFE*****************************************************/
+/*****************************************************ANGRIFFE*****************************************************/
+var wmove = new Array(); 
+
+wmove[0] = {
+    name: "default",
+    schaden: 3,
+    angriff: 2,
+}
+
+wmove[1] = {
+    name: "test1",
+    schaden: 3,
+    angriff: 2,
+}
+
+wmove[2] = {
+    name: "test2",
+    schaden: 3,
+    angriff: 2,
+}
